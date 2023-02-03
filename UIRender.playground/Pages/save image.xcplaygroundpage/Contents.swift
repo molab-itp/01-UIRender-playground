cf3db2a4c654dfb4c73992ccdd61b1b8da57f234
John Henry Thompson <jht1493@gmail.com>
2023-02-02 20:35:47 -0500

    Update Contents.swift

--- UIRender.playground/Pages/save image.xcplaygroundpage/Contents.swift
+++ UIRender.playground/Pages/save image.xcplaygroundpage/Contents.swift
@@ -9,10 +9,18 @@ let image = UIImage(systemName: "trash.circle.fill")!
 let data = image.pngData()
 
 let folder = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-let filePath = folder!.appendingPathComponent("trash.png");
+let filePath = folder!.appendingPathComponent("trash2.png");
 
 let err: ()? = try? data?.write(to: filePath)
 print("err \(String(describing: err))\nfilePath \(filePath)")
 
+// The file name begins with file://
+// the path follows this prefix
+
 // open filepath
 // cp --filePath-- ~/Downloads/.
+
+// Example Terminal command to copy the file to the Downloads folder
+// cp /Users/jht2/Library/Developer/XCPGDevices/21C4B76E-B237-4F4E-819B-039C6127A1A6/data/Containers/Data/Application/8ACBA674-5800-46FC-AA1B-35679C770D20/Documents/trash.png ~/Downloads/.
+//
+//
