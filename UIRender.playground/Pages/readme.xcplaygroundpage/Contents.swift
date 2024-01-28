db451582dc05101d2e2528fe73b4bb16813dd658
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2024-01-28 18:41:58 -0500

    readme

--- UIRender.playground/Pages/readme.xcplaygroundpage/Contents.swift
+++ UIRender.playground/Pages/readme.xcplaygroundpage/Contents.swift
@@ -1,7 +1,3 @@
-//: [Previous](@previous)
-
-import Foundation
-
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+//# [01-UIRender-playground](https://github.com/mobilelabclass-itp/01-UIRender-playground)
+                           
+// Techniques for creating image and saving images in Swift
